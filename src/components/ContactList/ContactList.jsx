1da56caedf7252e203301bbd0479769613fd5ac0
Tochka <tochka114@gmail.com>
2024-08-12 12:28:20 +0300

    new repo

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+import Contact from "./contact/Contact"
+
+ const ContactList = ({contacts,deleteContact}) => {
+
+  return (
+    <div>
+      {contacts.map(contact=>(
+        <Contact key={contact.id}
+         name={contact.name} 
+         number={contact.number}
+         id={contact.id}
+         deleteContact={deleteContact}
+         />
+      )) }  
+       
+
+    </div>
+  )
+}
+export default ContactList
